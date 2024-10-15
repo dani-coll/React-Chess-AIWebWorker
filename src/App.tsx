98e0872166886e286e007a7dd72256d1d0ce7843
Coll <daniel.coll@dynatrace.com>
2024-10-15 18:49:22 +0200

    Calculate initial best move on web worker

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,55 @@
+import { useMemo } from 'react';
 import './App.css';
 import Referee from './components/Referee/Referee';
+import { useWebWorker } from './worker/useWebWorker';
 
 function App() {
+  const boardConfiguration = {
+    "turn": "black",
+    "pieces": {
+        "E1": "K",
+        "C1": "B",
+        "E8": "k"
+    },
+    "moves": {
+      "E8": ["E7", "F8", "F7", "D8", "D7"]
+    },
+    "isFinished": false,
+    "check": false,
+    "checkMate": false,
+    "castling": {
+        "whiteLong": true,
+        "whiteShort": true,
+        "blackLong": true,
+        "blackShort": true    
+    },
+    "enPassant": "E6",
+    "halfMove": 0,
+    "fullMove": 1
+}
+
+  const workerInstance = useMemo(() => new Worker(new URL('./worker/chess-engine-worker', import.meta.url)), []);
+
+  const {
+    running,
+    error,
+    result,
+    startProcessing,
+  } = useWebWorker<any, any>(workerInstance);
+
+
+
+  console.log(result);
+
+  const start = () => {
+    startProcessing(boardConfiguration);
+  }
+
   return (
     <div id="app">
-      <Referee/>
+      <input></input>
+      <button onClick={start}> CLICK ME</button>
+      <Referee></Referee>
     </div>
   );
 }
---
+++ src/worker/chess-engine-worker.ts
@@ -0,0 +1,24 @@
+/* eslint-disable no-restricted-globals */
+/* eslint-disable import/no-anonymous-default-export */
+
+
+/* eslint-disable no-undef */
+
+
+const jsChessEngine = require('js-chess-engine')
+
+self.onmessage = (e) => {
+  try {
+    const { aiMove } = jsChessEngine;
+    console.log("Received: ", e.data)
+    const game = new jsChessEngine.Game()
+    const initialGame = game.exportJson();
+    console.log(initialGame)
+    const result = aiMove(initialGame, 3)
+
+    return postMessage({result});
+  } catch (error) {
+    console.log(error)
+    return postMessage({ error });
+  }
+};
